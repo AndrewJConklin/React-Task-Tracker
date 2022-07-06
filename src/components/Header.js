f9bf12e409521bc283d1ceaa94594d5ed3ca7bfe
aconkli9 <aconkli9@ford.com>
2022-07-06 16:19:33 -0400

    Add task adder button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import { PropTypes } from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = (e) => {
-    console.log('Click')
-  }
+const Header = ({ title, onAdd, showAdd }) => {
+
 
   return (
     <div className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </div >
   )
 }
 
 Header.defaultProps = {
-  title: "Task Track"
+  title: "Task Tracker"
 }
 
 Header.propTypes = {
